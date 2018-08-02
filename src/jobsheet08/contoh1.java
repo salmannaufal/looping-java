7f019105f867e058d17e30cd30836f31d8829b45
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-08-02 11:14:02 +0700

    for, while, do while

---
+++ src/jobsheet08/contoh1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet08;
+import java.util.Scanner;
+
+/**
+ *
+ * @author O M E N
+ */
+public class contoh1 {
+public static void main(String[] args)
+{
+    int x;
+    for(x = 1; x <= 5; x++)
+    {
+        System.out.println("SMK Telkom Malang");
+    }
+    
+    // PERULANGAN WHILE
+    int i = 1;
+    while(i <= 5)
+    {
+        System.out.println("SMK Telkom Malang");
+        i++;
+    }
+    // PERULANGAN DO WHILE
+    int j = 1;
+    do
+    {
+    System.out.println("SMK Telkom Malang");
+    j++;
+            
+    }
+    while(j >= 5);
+    
+   }
+}
+
+    
+    
+    
+
+
